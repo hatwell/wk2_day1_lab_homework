12310a32e1360745d3d2735285e6484ae5d04a06
hatwell <caroline.hatwell@gmail.com>
2017-03-13 20:50:37 +0000

    added title matching method and its tests.

--- lab_part_c.rb
+++ lab_part_c.rb
@@ -9,4 +9,18 @@ class Library
       puts book
     end
   end
+
+  def title_returns_details(title)
+    for book in @books
+      #In my feedback Zsolt said using a return statement
+      #inside a loop like this isnt great but I
+      #can't remember what he said to do instead!
+      if book[:title] == title
+        return book
+      end
+    end
+  end
+
 end
+
+# my_lib = Library.new([{author: "Caroline", title: "A great book"}, {author: "Harry", title: "Music is Good"}])
--- lab_part_c_spec.rb
+++ lab_part_c_spec.rb
@@ -3,41 +3,43 @@ require('minitest/rg')
 require_relative('./lab_part_c')
 
 class LibraryTest < MiniTest::Test
-  def setup
-    @my_books = [
-      {
-        title: "Bossypants",
-        author: "Tina Fey",
+
+    def setup
+      @my_books = [
+        {
+          title: "Bossypants",
+          author: "Tina Fey",
+          rental_details: {
+            student_name: "Caroline Hatwell",
+            due_date: "10/04/2017"
+          }
+        },
+        {
+          title: "The Pragmatic Programmer",
+          author: "Andy Hunt",
+          rental_details: {
+            student_name: "Dominic Fraser",
+            due_date: "14/04/2017"
+          }
+        },
+        {
+        title: "Information Architecture",
+        author: "O'Reilly",
         rental_details: {
-          student_name: "Caroline Hatwell",
-          due_date: "10/04/2017"
+          student_name: "Tristan Grey",
+          due_date: "05/01/2017"
         }
-      },
-      {
-        title: "The Pragmatic Programmer",
-        author: "Andy Hunt",
-        rental_details: {
-          student_name: "Dominic Fraser",
-          due_date: "14/04/2017"
-        }
-      },
-      {
-      title: "Information Architecture",
-      author: "O'Reilly",
-      rental_details: {
-        student_name: "Tristan Grey",
-        due_date: "05/01/2017"
       }
-    }
-    ]
-    @my_library = Library.new(@my_books)
-  end
+      ]
+      @my_library = Library.new(@my_books)
+    end
 
-  def test_print_book_details
-    assert_equal(@my_books, @my_library.print_book_details)
-  end
+    def test_print_book_details
+      assert_equal(@my_books, @my_library.print_book_details)
+    end
 
-  def test_title_returns_details
-    
-  end
+    def test_title_returns_details
+      bossypants_details = @my_library.books[0]
+      assert_equal(bossypants_details, @my_library.title_returns_details("Bossypants"))
+    end
 end
