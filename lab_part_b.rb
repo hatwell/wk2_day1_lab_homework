eab036dc5029c54493f03718ff2367dd51bbe367
hatwell <caroline.hatwell@gmail.com>
2017-03-13 15:45:44 +0000

    refactored to use attr_accessor and tested this

--- lab_part_b.rb
+++ lab_part_b.rb
@@ -1,20 +1,32 @@
 class SportsTeam
+
+  attr_accessor :team_name, :players, :coach
+
   def initialize(team_name, players, coach)
     @team_name = team_name
     @players = players
     @coach = coach
+    @points = 0
   end
 
-  def team_name
-    @team_name
+  def add_player(player)
+    @players << player
   end
 
-  def players
-    @players
-  end
-
-  def set_coach_name(new_coach_name)
-    @coach = new_coach_name
-  end
+  # def team_name
+  #   @team_name
+  # end
+  #
+  # def players
+  #   @players
+  # end
+  #
+  # def coach
+  #   @coach
+  # end
+  #
+  # def set_coach_name(new_coach_name)
+  #   @coach = new_coach_name
+  # end
 
 end
--- lab_part_b_spec.rb
+++ lab_part_b_spec.rb
@@ -7,14 +7,35 @@ class SportsTeamTest < MiniTest::Test
     @team = SportsTeam.new("Cleveland Cavaliers", ["LeBron James", "Kyrie Irving", "JR Smith", "Kevin Love"], "Tyronn Lue")
   end
 
-  def test_name_getter
-    assert_equal("Cleveland Cavaliers", @team.team_name)
+  # def test_name_getter
+  #   assert_equal("Cleveland Cavaliers", @team.team_name)
+  # end
+  #
+  # def test_team_players_getter
+  #   assert_equal(["LeBron James",
+  #                 "Kyrie Irving",
+  #                 "JR Smith",
+  #                 "Kevin Love"], @team.players)
+  # end
+  #
+  # def test_coach_getter
+  #   assert_equal("Tyronn Lue", @team.coach)
+  # end
+  #
+  # def test_coach_setter
+  #   @team.set_coach_name("Steve Kerr")
+  #   assert_equal("Steve Kerr", @team.coach)
+  # end
+
+  def test_attr_accessor_coach
+    @team.coach = "Steve Kerr"
+    assert_equal("Steve Kerr", @team.coach)
   end
 
-  def test_team_players_getter
-    assert_equal(["LeBron James",
-                  "Kyrie Irving",
-                  "JR Smith",
-                  "Kevin Love"], @team.players)
+  def test_add_player
+    @team.add_player("Channing Frye")
+    assert_equal(@team.players.last, "Channing Frye")
   end
+
+
 end
