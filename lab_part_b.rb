ada40c5d775aab734d1690bdad72ea111663c876
hatwell <caroline.hatwell@gmail.com>
2017-03-13 15:31:57 +0000

    added getter methods

--- lab_part_b.rb
+++ lab_part_b.rb
@@ -1,3 +1,20 @@
 class SportsTeam
+  def initialize(team_name, players, coach)
+    @team_name = team_name
+    @players = players
+    @coach = coach
+  end
+
+  def team_name
+    @team_name
+  end
+
+  def players
+    @players
+  end
+
+  def set_coach_name(new_coach_name)
+    @coach = new_coach_name
+  end
 
 end
--- lab_part_b_spec.rb
+++ lab_part_b_spec.rb
@@ -3,5 +3,18 @@ require('minitest/rg')
 require_relative('./lab_part_b')
 
 class SportsTeamTest < MiniTest::Test
-  
+  def setup
+    @team = SportsTeam.new("Cleveland Cavaliers", ["LeBron James", "Kyrie Irving", "JR Smith", "Kevin Love"], "Tyronn Lue")
+  end
+
+  def test_name_getter
+    assert_equal("Cleveland Cavaliers", @team.team_name)
+  end
+
+  def test_team_players_getter
+    assert_equal(["LeBron James",
+                  "Kyrie Irving",
+                  "JR Smith",
+                  "Kevin Love"], @team.players)
+  end
 end
