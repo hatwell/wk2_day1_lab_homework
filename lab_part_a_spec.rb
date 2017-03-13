8d3868fe4f3085e43713848dc9a4144137e22ad4
hatwell <caroline.hatwell@gmail.com>
2017-03-13 14:55:01 +0000

    wrote test name function

--- lab_part_a_spec.rb
+++ lab_part_a_spec.rb
@@ -7,7 +7,12 @@ class StudentTest < MiniTest::Test
   def setup
     @student_1 = Student.new("Drake", 13)
     @student_2 = Student.new("Zlatan", 14)
-  end 
+  end
+
+  def test_get_name
+    assert_equal("Drake", @student_1.name)
+    assert_equal("Zlatan", @student_2.name)
+  end
 
 
 end
